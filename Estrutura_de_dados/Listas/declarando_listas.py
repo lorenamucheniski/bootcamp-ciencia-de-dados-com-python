dce958b3b69de2082a8122e2ef60ff4b03cc0095
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-21 06:26:55 -0300

    Trabalhando estruturas de dados

---
+++ Estrutura_de_dados/Listas/declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maçâ", "uva", "morango"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list ('python')
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 420000 , 2023, "São Paulo", True]
+print(carro)
