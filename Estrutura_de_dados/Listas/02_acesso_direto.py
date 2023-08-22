66ccebed9531bbac562a88c9bc585d93e8bfcdfe
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-22 06:15:18 -0300

    Acesso direto a listas

---
+++ Estrutura_de_dados/Listas/02_acesso_direto.py
@@ -0,0 +1,6 @@
+# acessando itens na lista de forma direta
+
+frutas = ['maçã', 'banana', 'uva','pera']
+
+print(frutas[0])   #mostre a fruta que está no índice 0
+print(frutas[3])   #mostre fruta que está no índice 3
