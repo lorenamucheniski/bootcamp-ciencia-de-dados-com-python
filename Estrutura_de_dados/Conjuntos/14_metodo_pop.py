2ac9683c03593bba4a2028e2105bd428bd5a96a8
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-26 07:32:20 -0300

    Métod pop

---
+++ Estrutura_de_dados/Conjuntos/14_metodo_pop.py
@@ -0,0 +1,13 @@
+numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros)
+
+print(numeros.pop())
+print(numeros)
+print(numeros.pop())
+print(numeros)
+print(numeros.pop())
+print(numeros)
+print(numeros.pop())
+print(numeros)
+print(numeros.pop())
+print(numeros)
