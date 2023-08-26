c15ad7ef679fc5d500e19c5aae2557f11b8678f5
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-26 14:12:23 -0300

    Criando dicionários

---
+++ Estrutura_de_dados/Dicionarios/01_criando_dicionarios.py
@@ -0,0 +1,7 @@
+pessoa = {'nome' : 'Guilherme', 'idade': 28}
+
+pessoa = dict(nome = 'Guilherme', idade = 28)
+
+pessoa['telefone'] = '332-1569' #para adicionar mais um valor no nosso dicionário
+
+print(pessoa)
