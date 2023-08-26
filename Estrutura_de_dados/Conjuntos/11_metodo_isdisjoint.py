548c4b7bc5e3414e5e5b642a359e8f2a2233676e
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-26 07:26:36 -0300

    Método isdisjoint

---
+++ Estrutura_de_dados/Conjuntos/11_metodo_isdisjoint.py
@@ -0,0 +1,7 @@
+conjunto_a = {1, 2, 3, 4}
+conjunto_b = {5, 6, 7, 8}
+conjunto_c = {1, 0}
+
+print(conjunto_a.isdisjoint(conjunto_b))
+print(conjunto_a.isdisjoint(conjunto_c))
+print(conjunto_b.isdisjoint(conjunto_c))
