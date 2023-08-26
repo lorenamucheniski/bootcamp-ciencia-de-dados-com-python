764818c3e14b971deeab9032f1b75f598a157072
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-26 13:46:04 -0300

    .

--- Estrutura_de_dados/Listas/indices_negativos.py
+++
@@ -1,6 +0,0 @@
-# acessando itens na lista de forma negativa 
-
-frutas = ['maçã', 'banana', 'uva','pera']
-
-print(frutas[-1])   #mostre a fruta que está no índice -1 (da direita para a esquerda)
-print(frutas[-3])   #mostre a fruta que está no índice -3 (da direita para a esquerda)
