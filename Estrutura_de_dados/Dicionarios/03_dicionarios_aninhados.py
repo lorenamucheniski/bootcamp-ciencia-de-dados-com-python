82b60e5c84bd88e85b26e0f340d321577fdd13c4
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-29 06:01:47 -0300

    Aninhando dicionários

---
+++ Estrutura_de_dados/Dicionarios/03_dicionarios_aninhados.py
@@ -0,0 +1,8 @@
+contatos = {
+    'guilherme@gmail.com' : {'nome': 'Guilherme', 'telefone': '3333-2221'},
+    'giovanna@gmail.com' : {'nome': 'Giovanna', 'telefone': '3333-2121'},
+    'chappie@gmail.com' : {'nome': 'Chappie', 'telefone': '3344-1871'},
+    'Melaine@gmail.com' : {'nome': 'Melaine', 'telefone': '3333-7766'},
+}
+
+print(contatos['giovanna@gmail.com']['telefone'])
