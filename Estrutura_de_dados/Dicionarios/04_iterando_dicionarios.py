1b68a7ae23261aaf024dfdb74ccf5cf53714635f
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-29 06:02:10 -0300

    Iterando dicionários

---
+++ Estrutura_de_dados/Dicionarios/04_iterando_dicionarios.py
@@ -0,0 +1,9 @@
+contatos = {
+    'guilherme@gmail.com' : {'nome': 'Guilherme', 'telefone': '3333-2221'},
+    'giovanna@gmail.com' : {'nome': 'Giovanna', 'telefone': '3333-2121'},
+    'chappie@gmail.com' : {'nome': 'Chappie', 'telefone': '3344-1871'},
+    'Melaine@gmail.com' : {'nome': 'Melaine', 'telefone': '3333-7766'},
+}
+
+for chave, valor in contatos.items():
+    print(chave, valor)
