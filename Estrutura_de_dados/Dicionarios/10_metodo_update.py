c590e72964f916fbb4602b44859d6865457624f4
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-30 06:12:45 -0300

    Método update

---
+++ Estrutura_de_dados/Dicionarios/10_metodo_update.py
@@ -0,0 +1,7 @@
+dados = {'guilherme@gmail.com' : {'nome': 'Guilherme', 'telefone': '3333-2221'}}
+
+dados.update({'guilherme@gmail.com': {'nome':'Gui'}})
+print(dados)
+
+dados.update({'lorena@gmail.com':{'nome': 'Lorena', 'telefone': '9658-4745', 'idade': 25}})
+print(dados)
