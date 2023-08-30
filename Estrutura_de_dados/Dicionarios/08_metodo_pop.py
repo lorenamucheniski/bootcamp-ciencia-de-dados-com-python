3aebab4a877434ef4c272104ad1e6f6cb47d2447
lorenamucheniski <lomucheniski25@gmail.com>
2023-08-30 06:11:58 -0300

    Método pop

---
+++ Estrutura_de_dados/Dicionarios/08_metodo_pop.py
@@ -0,0 +1,5 @@
+dados = {'guilherme@gmail.com' : {'nome': 'Guilherme', 'telefone': '3333-2221'}}
+
+print(dados.pop('guilherme@gmail.com'))
+
+print(dados.pop('guilherme@gmail.com', 'não encontrado'))
