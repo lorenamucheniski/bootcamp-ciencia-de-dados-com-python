f0a8d49f0d06a1c975883d915cf2f0e0a40196a0
lorenamucheniski <lomucheniski25@gmail.com>
2023-09-01 16:37:48 -0300

    Parâmetros por posição

---
+++ Estrutura_de_dados/Funcoes/05_parametros_por_posicao.py
@@ -0,0 +1,5 @@
+def criar_carro(modelo, ano, placa, / , marca, motor, combustível):
+    print(modelo, ano, placa, marca, motor, combustível)
+
+criar_carro('Palio', 1999, 'ABC-1234', marca= 'Fiat', motor= '1.0', combustível= 'Gasolina' )  
+
