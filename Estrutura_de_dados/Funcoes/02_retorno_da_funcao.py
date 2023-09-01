c4a95f8bffcb6f23a2f3e8d1953dd0e3f6650e18
lorenamucheniski <lomucheniski25@gmail.com>
2023-09-01 10:59:40 -0300

    Retornando uma função

---
+++ Estrutura_de_dados/Funcoes/02_retorno_da_funcao.py
@@ -0,0 +1,13 @@
+def calcular_total(numeros):
+    return sum(numeros)
+
+print(calcular_total([10, 20, 15, 200]))
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero -1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(retorna_antecessor_e_sucessor(10))
+
