1b5a16a4104f77fb6d1f155bab9cec80d385152e
lorenamucheniski <lomucheniski25@gmail.com>
2023-09-02 15:53:13 -0300

    Parametros por nome

---
+++ Estrutura_de_dados/Funcoes/06_parametros_por_nome.py
@@ -0,0 +1,5 @@
+def criar_carro(*, modelo, ano, placa, marca, motor, combustível):
+    print(modelo, ano, placa, marca, motor, combustível)
+
+criar_carro(modelo= 'Palio', ano= 1999, placa= 'ABCD-1234', marca= 'Fiat', motor= '1.0', combustível= 'Gasolina') 
+
