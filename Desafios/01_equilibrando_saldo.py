96329482953c364cc11c0f9d212b26ef6c1bce71
lorenamucheniski <lomucheniski25@gmail.com>
2023-09-03 18:37:25 -0300

    Equilibrando saldo

---
+++ Desafios/01_equilibrando_saldo.py
@@ -0,0 +1,8 @@
+saldo_atual = float(input())
+valor_deposito = float(input())
+valor_retirada = float(input())
+
+saldo_atualizado = (saldo_atual + valor_deposito) - valor_retirada
+
+print('Saldo atualizado na conta: {}'.format (saldo_atualizado))
+
